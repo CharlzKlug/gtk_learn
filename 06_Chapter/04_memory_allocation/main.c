2be30349bd0c770ac1d374ffc4f1ac048fc5b9c0
Imil Ametov <charlz_klug@mail.ru>
2023-07-12 14:46:39 +0300

    Added comments to Memory Allocation example

--- 06_Chapter/04_memory_allocation/main.c
+++ 06_Chapter/04_memory_allocation/main.c
@@ -1,40 +1,42 @@
 #include <glib.h>
 #include <stdio.h>
 
+/* Структура, с которой будем работать */
 struct myStructure {
   int k;
   double f;
 };
 
 int main() {
+
+  /* Выделяем память под две структуры */
   struct myStructure* ms_array= NULL;
   ms_array= g_new0(struct myStructure, 2);
 
-  /* ms1->k= 1; */
-  /* ms1->f= 42.3; */
-
-  /* printf("k: %d, f: %f\n", ms1->k, ms1->f); */
-
+  /* Заполняем структуры */
   ms_array[0].k= 1;
   ms_array[0].f= 42.3;
 
   ms_array[1].k= 2;
   ms_array[1].f= 15.37;
 
+  /* Печатаем содержимое структур */
   for (int i= 0; i < 2; i++) {
     printf("[%d], k: %d, f: %f\n", i, ms_array[i].k, ms_array[i].f);
   }
 
-  /* ((char*)ms_array)= 255; */
+  /* Заполняем содержимое структуры мусором перед освобождением памяти */
   char* chr= NULL;
   chr= (char*)ms_array;
 
   for (int i=0; i < sizeof(struct myStructure) * 2; i++) {
     chr[i]= 255;
   }
-  
+
+  /* Освобождаем память */
   g_free(ms_array);
-  
+
+  /* ЗаNULL-яем указатели */
   ms_array= NULL;
   chr= NULL;
   
