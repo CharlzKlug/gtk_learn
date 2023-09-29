06da74f7ff436c575f78dff7b35f8bbfc2f4e677
Imil Ametov <charlz_klug@mail.ru>
2023-09-29 11:05:53 +0300

    Added textview sample

---
+++ 07_Chapter/250_textview/textview.c
@@ -0,0 +1,25 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+  GtkWidget *window, *scrolled_win, *textview;
+  GtkTextBuffer *buffer;
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Text Views");
+  gtk_container_set_border_width(GTK_CONTAINER(window), 0);
+  gtk_widget_set_size_request(window, 250, 150);
+
+  textview = gtk_text_view_new();
+  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
+  gtk_text_buffer_set_text(buffer, "Your 1st GtkTextView widget!", -1);
+
+  scrolled_win = gtk_scrolled_window_new(NULL, NULL);
+  gtk_container_add(GTK_CONTAINER(scrolled_win), textview);
+  gtk_container_add(GTK_CONTAINER(window), scrolled_win);
+  gtk_widget_show_all(window);
+
+  gtk_main();
+  return 0;
+}
